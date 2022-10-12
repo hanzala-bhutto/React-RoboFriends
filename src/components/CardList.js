59f4ef680e2c0c0a3430144aa4cf0b2778b9e3b1
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-12 19:19:59 +0500

    cardList component key unique identifier added for each card component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from './Card';
 
-// Cardlist component that generates multiple Card components including robot information
+// Cardlist component that generates multiple Card components each containing robot information
 const CardList = ({robots}) => {
     return (
-        robots.map((robot) => {
-            return <Card id={robot.id} name={robot.name} email={robot.email}/>
+        // it is necessary to define key for each Card Component created so that Virtual Dom can keep track of it
+        robots.map((robot,i) => {
+            return <Card key={i} id={robot.id} name={robot.name} email={robot.email}/>
         })
     );
 }
