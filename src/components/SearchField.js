6fd6232df40a31ae57d45447e63d6277ef38f4d1
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-12 18:43:20 +0500

    SearchField Component template

---
+++ src/components/SearchField.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchField = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default SearchField;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,10 +3,38 @@ import { Component } from 'react';
 import 'tachyons';
 import CardList from '../components/CardList';
 
+// main app class which is the first one to be build and rendered
 class App extends Component {
+
+  // this allows us to set state so that state fields can be set or updated dynamically
+  constructor(){
+    super();
+    this.state = {
+      "robots": [],
+      "searchField" : ""
+    }
+  }
+
+  // runs when app is rendered
+  componentDidMount(){
+    // fetch robots info using api endpoint then update the robots state
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots : users}))
+  }
+
+  searchOnChange(){
+
+  }
+
+  // renders the main app
   render(){
     return (
-      <CardList />
+      <div>
+        <h1>RoboFriends</h1>
+          <SeachField searchField={searchOnChange}/>
+        <CardList robots={this.state.robots}/>
+      </div>
     );
   }
 }
