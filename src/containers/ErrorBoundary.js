880c679f1be23b4b4670ea2e734ee426f2c80644
Bhutto <100479481+hanzala-bhutto@users.noreply.github.com>
2022-10-15 23:51:14 +0500

    error boundary to catch if cardlist component fails to load

--- src/containers/ErrorBoundary.js
+++ src/containers/ErrorBoundary.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 
+// catches error inside those 
 class ErrorBoundary extends Component {
+    constructor(){
+        super();
+        this.state = {
+            hasError: false
+        }
+    }
+
+    componentDidCatch(){
+        this.setState({hasError:true});
+    }
+
+    render(){
+        return this.state.hasError ?
+        <h1>The CardList Component failed to load</h1> :
+        this.props.children;
+    }
 
 }
 
